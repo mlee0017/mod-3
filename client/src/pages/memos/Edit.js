b89b72eece548a7c10358a652be8e5ca3b7ea9a3
Michelle Lee <lee.mxvii@gmail.com>
2023-04-05 23:06:27 -0700

    added options in edit

--- client/src/pages/memos/Edit.js
+++ client/src/pages/memos/Edit.js
@@ -28,9 +28,29 @@ function Edit() {
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="mood">mood:</label><br />
                     <input type="text" id="mood" ref={moodRef} defaultValue={memo.mood} /><br /><br />
-
-
+                    <select
+                        class="form-select"
+                        aria-label="Default select example"
+                        name="mood"
+                    >
+                    <option selected>mood:</option>
+                    <option value="(づ￣ ³￣)づ">(づ￣ ³￣)づ</option>
+                    <option value="(｡¬‿¬｡)">(｡¬‿¬｡)</option>
+                    <option value="┬──┬ /( ゜-゜/)">┬──┬ /( ゜-゜/)</option>
+                    <option value="(╯°□°）╯︵ ┻━┻">(╯°□°）╯︵ ┻━┻</option>
+                    <option value="(ಥ﹏ಥ)">(ಥ﹏ಥ)</option>
+                    <option value="ᕦ(ò_óˇ )ᕤ">ᕦ(ò_óˇ )ᕤ</option>
+                    <option value="┴┬┴┤(･_├┬┴┬">┴┬┴┤(･_├┬┴┬</option>
+                    <option value="(;´༎ຶД༎ຶ`)">(;´༎ຶД༎ຶ`)</option>
+                    <option value="(♥‿♥ ✿)">(♥‿♥ ✿)</option>
+                    <option value="(ಠ╭╮ಠ)">(ಠ╭╮ಠ)</option>
+                    <option value="(◕‿◕✿)">(◕‿◕✿)</option>
+                    <option value="( ⚆ _ ⚆ )">( ⚆ _ ⚆ )</option>
+                    <option value="(~˘▾˘)~">(~˘▾˘)~</option>
+                    </select>
+                    <br /><br /> 
                     <button>Submit</button>
+                    <br /> 
                 </form>
                 <Link to={`/memo/${memo._id}`}>
                     <button>Back</button>
